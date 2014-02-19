7cb2b2846d0c503b85ffc82c4c48c3cae083cddc
Jason <test>
2014-02-18 23:33:43 -0600

    1. Added servlets to handle adding to the datastore objects 2. Added servlet mappings to web.xml

--- AdviseMe/src/webapp/datastoreObjects/User.java
+++ AdviseMe/src/webapp/datastoreObjects/User.java
@@ -1,14 +1,21 @@
 package webapp.datastoreObjects;
 
+import java.util.ArrayList;
+
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
 @Entity
 public class User {
 	@Id String name = "No username entered.";
+	ArrayList<Course> courseList;
+	ArrayList<Course> currentCourses;
 	
 	@SuppressWarnings("unused")
-	private User(){}
+	private User(){
+		courseList = new ArrayList<Course>();
+		currentCourses = new ArrayList<Course>();
+	}
 	
 	public User(String name){
 		this.name=name;
---
+++ AdviseMe/war/addcourse.jsp
@@ -0,0 +1,10 @@
+<html>
+	<body>
+		<form name="example1"><div align="center">
+		<select name="select1" size="1">
+		<option>Red</option>
+		<option>Blue</option>
+		<option>Green</option>
+		</select></div></form>
+	</body>
+</html>
