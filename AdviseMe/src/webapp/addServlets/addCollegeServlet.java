4c5b670a6cd7d92ca02435225270c1a304d67e83
Jason <jason.anthraper@utexas.edu>
2014-02-20 17:55:22 -0600

    Testing to see if name will show up correctly on GitHub website.

--- AdviseMe/src/webapp/addServlets/addCollegeServlet.java
+++ AdviseMe/src/webapp/addServlets/addCollegeServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
+
 @SuppressWarnings("serial")
 public class addCollegeServlet extends HttpServlet{
 	static{ObjectifyService.register(School.class);}
