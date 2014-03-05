d84f50b43b246b8dac3bfe6fed679edf90e935b2
Jason <jason.anthraper@utexas.edu>
2014-03-05 15:46:12 -0600

    Added email functionality.

--- AdviseMe/src/webapp/serviceServlets/emailServlet.java
+++ AdviseMe/src/webapp/serviceServlets/emailServlet.java
@@ -16,6 +16,35 @@ import javax.servlet.http.HttpServletResponse;
 
 import webapp.datastoreObjects.*;
 
-public class emailServlet {
+public class emailServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		try{
+			MimeMessage message = new MimeMessage(session, req.getInputStream());
+			Address[] fromAddresses = message.getFrom();
+			String subject = message.getSubject();
+			String strCallResult ="";
+			try{
+				strCallResult = "Sorry, This email address is automated.";
+				Message msg = new MimeMessage(session);
+				msg.setFrom(new InternetAddress("utadviseme@gmail.com", "Advise Me Automated Email"));
+				msg.addRecipient(Message.RecipientType.TO, new InternetAddress(fromAddresses[0].toString()));
+				msg.setSubject("Error");
+				msg.setText(strCallResult);
+				Transport.send(msg);
+			}catch(Exception e){
+				Message msg = new MimeMessage(session);
+				msg.setFrom(new InternetAddress("utadviseme@gmail.com", "Advise Me Automated Email"));
+				msg.addRecipient(Message.RecipientType.TO, new InternetAddress(fromAddresses[0].toString()));
+				msg.setSubject("Error");
+				msg.setText("Something wrong happened:(");
+				Transport.send(msg);
+			}
+		}catch(MessagingException e){
+			//something happened.
+		}
+	}
 
 }
