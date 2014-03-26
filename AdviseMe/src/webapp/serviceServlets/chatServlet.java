156d28d29299ef5898b4d395fa1dfae856e7eb40
Jason <jason.anthraper@utexas.edu>
2014-03-26 00:31:25 -0500

    Updated chat servlet to be useful.

--- AdviseMe/src/webapp/serviceServlets/chatServlet.java
+++ AdviseMe/src/webapp/serviceServlets/chatServlet.java
@@ -1,6 +1,10 @@
 package webapp.serviceServlets;
 
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +17,7 @@ import com.google.appengine.api.xmpp.MessageBuilder;
 import com.google.appengine.api.xmpp.SendResponse;
 import com.google.appengine.api.xmpp.XMPPService;
 import com.google.appengine.api.xmpp.XMPPServiceFactory;
+import com.googlecode.objectify.ObjectifyService;
 
 import webapp.datastoreObjects.*;
 
@@ -36,16 +41,24 @@ public class chatServlet extends HttpServlet{
 			if(strCommand.length()==0){
 				throw new Exception("You must give a command.");
 			}
-			String[] words = strCommand.split(" ");
-			if(words.length==1){
+			String[] words = strCommand.split("@");
+			if(words.length>=1){
 				if(words[0].equalsIgnoreCase("help")){
 					StringBuffer SB = new StringBuffer();
 					SB.append("******Help*******");
 					SB.append("you asked for help.");
 					SB.append("you will get no help");
 					strCallResult = SB.toString();
+				}else if(words[0].equalsIgnoreCase("addcourse")){
+					String[]courseInfo = words[1].split("#");
+					boolean flag = addCourse(courseInfo[0],courseInfo[1],courseInfo[2],courseInfo[3],courseInfo[4],courseInfo[5],courseInfo[6]);
+					if(flag){
+						strCallResult = "Congrats. You actually got something done.";
+					}else{
+						strCallResult = "You done goofed. Something happened. Blame Jason.";
+					}
 				}else{
-					strCallResult = "Sorry! I don't understand english.";
+					strCallResult = "Much English. Very understand. Doge Unhappy.";
 				}
 			}
 			boolean messageSent = false;
@@ -62,5 +75,46 @@ public class chatServlet extends HttpServlet{
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
+	}
+	
+	static{ObjectifyService.register(Course.class);}
+	public boolean addCourse(String courseName,String courseTitle,String courseDescription,String upperDivision,String professorList, String semesterTaught, String textbooks){
+				
+		if(courseName==null||courseName.isEmpty()){
+			return false;
+		}
+		if(courseTitle==null||courseTitle.isEmpty()){
+			return false;
+		}
+		if(courseDescription==null||courseDescription.isEmpty()){
+			return false;		
+		}
+		if(upperDivision==null||upperDivision.isEmpty()){
+			return false;		
+		}
+		if(professorList==null||professorList.isEmpty()){
+			return false;		
+		}
+		if(semesterTaught==null||semesterTaught.isEmpty()){
+			return false;		
+		}
+		if(textbooks==null||textbooks.isEmpty()){
+			return false;	
+		}
+		List<Course> schoolList=ObjectifyService.ofy().load().type(Course.class).list();
+		Collections.sort(schoolList);
+		boolean upper;
+		if(upperDivision.equals("upper")){
+			upper = true;
+		}else{
+			upper=false;
+		}
+		Course course = new Course(courseName,courseTitle,courseDescription,upper);
+		//TODO: Need to parse the list correctly and add the professors correctly
+		course.getProfessorList().add(professorList);
+		course.getSemesterTaught().add(semesterTaught);
+		course.getTextbooks().add(textbooks);
+		ofy().save().entity(course).now();
+		return true;
 	}
 }
